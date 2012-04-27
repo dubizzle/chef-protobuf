260d23788ddda7a9aad72198d8c91b1acfcd4724
Aaron D. Valade <adv@alum.mit.edu>
2012-04-27 20:07:43 +0800

    corrected the license and added operating system supportfixed an incorrect link to the readme

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,10 @@
 maintainer       "GeneralSensing LTD"
 maintainer_email "aaron@generalsensing.com"
-license          "All rights reserved"
+license          "Apache 2.0"
 description      "Installs/Configures Google Protocol Buffers"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+supports "ubuntu", ">= 8.10"
 
 depends "build-essential"
